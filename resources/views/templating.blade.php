43ec810aa2b0a5923814d3e506f40a87f886018b
maulibrahim20 <maulibrahim20@student.polindra.ac.id>
2022-11-02 01:32:15 +0700

    update

---
+++ resources/views/templating.blade.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<title>Atlantis Lite - Bootstrap 4 Admin Dashboard</title>
+	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+	
+	@include("komponen.css.style_css")
+
+</head>
+<body>
+	<div class="wrapper">
+		@include("komponen.header.header")
+
+		<!-- Sidebar -->
+		@include("komponen.sidebar.side_bar")
+		<!-- End Sidebar -->
+
+		<div class="main-panel">
+			<div class="content">
+				@yield("content")
+			</div>
+			<footer class="footer">
+				<div class="container-fluid">
+					<div class="copyright ml-auto">
+						2018, made with <i class="fa fa-heart heart text-danger"></i> by <a href="https://www.themekita.com">ThemeKita</a>
+					</div>				
+				</div>
+			</footer>
+		</div>
+		
+		<!-- Custom template | don't include it in your project! -->
+		<!-- End Custom template -->
+	</div>
+	@include("komponen.js.style_js")
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/templating", function(){
+    return view("/templating");
+});
+
+Route::get("/dashboard", [AppController::class,"dashboard"]);
